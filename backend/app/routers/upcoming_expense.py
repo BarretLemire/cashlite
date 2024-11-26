514ce4b679ce0988391430ea408e39b7a6a72ca1
Barret Lemire <blemire4503@xmail.dixietech.edu>
2024-11-25 23:09:16 -0700

    Add backend integration with upcoming and calendar

--- backend/app/routers/upcoming_expense.py
+++ backend/app/routers/upcoming_expense.py
@@ -7,17 +7,18 @@ from app.auth import get_current_active_user
 
 router = APIRouter()
 
-@router.post("/upcoming_expenses/")
+@router.post("/")
 def create_upcoming_expense(upcoming_expense: schemas.UpcomingExpenseCreate, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_active_user)):
     return crud.create_upcoming_expense(db=db, user_id=current_user.id, **upcoming_expense.dict())
 
-@router.get("/upcoming_expenses/")
+@router.get("/")
 def read_upcoming_expenses(db: Session = Depends(get_db), current_user: models.User = Depends(get_current_active_user)):
     return crud.get_upcoming_expenses_by_user(db=db, user_id=current_user.id)
 
-@router.delete("/upcoming_expenses/{upcoming_expense_id}")
+@router.delete("/{upcoming_expense_id}")
 def delete_upcoming_expense(upcoming_expense_id: int, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_active_user)):
     success = crud.delete_upcoming_expense(db=db, upcoming_expense_id=upcoming_expense_id)
     if not success:
         raise HTTPException(status_code=404, detail="Upcoming Expense not found")
     return {"detail": "Upcoming Expense deleted"}
+
